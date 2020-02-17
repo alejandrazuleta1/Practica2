0b1a1f97ea4a80410796cf286bf2effb10f8c528
Alejandra <alejandra.zuletag1@udea.edu.co>
2020-02-17 00:38:42 -0500

    practica 3 add navegation view

--- app/src/main/java/com/alejandrazuleta/clase2/MainActivity.kt
+++ app/src/main/java/com/alejandrazuleta/clase2/MainActivity.kt
@@ -1,19 +1,27 @@
 package com.alejandrazuleta.clase2
 
-import android.app.Activity
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import android.view.Menu
+import android.content.Intent
 import android.view.MenuItem
-import android.widget.Toast
-import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     var correo =""
     var password =""
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +31,36 @@ class MainActivity : AppCompatActivity() {
         if (datosRecebidos != null){
             correo = datosRecebidos?.getString("correo").toString()
             password = datosRecebidos?.getString("password").toString()
-            tv_resultado.text = correo
         }
+
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.nav_perfil, R.id.nav_notas, R.id.nav_fechacitacion,
+                R.id.nav_horario, R.id.nav_calendario, R.id.nav_calculadora,
+                R.id.nav_cerrarsesion
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.activity_main_drawer, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
     override fun onBackPressed() {
@@ -32,23 +68,14 @@ class MainActivity : AppCompatActivity() {
         finish()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu_overflow,menu)
-        return true
-    }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if(item.itemId == R.id.i_cerrar){
+        if(item.itemId == R.id.nav_cerrarsesion){
             var intent = Intent(this,LoginActivity::class.java)
             intent.putExtra("email",correo)
             intent.putExtra("password",password)
             startActivity(intent)
             finish()
         }
-
         return super.onOptionsItemSelected(item)
     }
-
-
 }
-
