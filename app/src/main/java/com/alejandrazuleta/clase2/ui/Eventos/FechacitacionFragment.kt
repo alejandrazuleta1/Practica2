3cd748f245cfe43544ba82a6d37af837ba2c7aef
Alejandra <alejandra.zuletag1@udea.edu.co>
2020-03-16 22:31:22 -0500

    añadidas las interfaces de calculadora

--- app/src/main/java/com/alejandrazuleta/clase2/ui/Eventos/FechacitacionFragment.kt
+++ app/src/main/java/com/alejandrazuleta/clase2/ui/Eventos/FechacitacionFragment.kt
@@ -1,11 +1,20 @@
 package com.alejandrazuleta.clase2.ui.Eventos
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.alejandrazuleta.clase2.Practica2
 import com.alejandrazuleta.clase2.R
+import com.alejandrazuleta.clase2.model.Usuario
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.fragment_fechacitacion.*
 
 class FechacitacionFragment : Fragment() {
 
@@ -13,8 +22,49 @@ class FechacitacionFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fechacitacion, container, false)
+        val view =inflater.inflate(R.layout.fragment_fechacitacion, container, false)
+
+        return view
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val usuarioDAO = Practica2.database.UsuarioDAO()
+
+        var miUsuario : Usuario?
+        if (usuarioDAO.loadAllUsers().size>0) {
+            //poner datos guardados en room
+            miUsuario = usuarioDAO.loadAllUsers()[0]
+            //mostrar datos guardados de ROOM si hay
+            tv_fecha.text = miUsuario.citacion
+        }
+
+        val auth = FirebaseAuth.getInstance()
+        val user = auth.currentUser
+        if (user != null) {
+            // already signed in
+            val database = FirebaseDatabase.getInstance()
+            val myRef = database.getReference("usuarios")
+
+            // Read from the database
+            myRef.addValueEventListener(object : ValueEventListener {
+                override fun onDataChange(dataSnapshot: DataSnapshot) {
+                    for (postSnapshot in dataSnapshot.children) {
+                        if(postSnapshot.key==user.uid){
+                            miUsuario= postSnapshot.getValue(Usuario::class.java)
+                            tv_fecha.text = miUsuario!!.citacion
+                            break
+                        }
+                    }
+                }
+                override fun onCancelled(error: DatabaseError) {
+                    // Failed to read value
+                    Log.w("ReadV", "Failed to read value.", error.toException())
+                }
+            })
+        }
+
     }
 
 
